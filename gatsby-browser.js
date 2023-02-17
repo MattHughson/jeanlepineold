89da160bdfce38f596c6d1f192ea05f33794cfb0
Matt Hughson <50377442+MattHughson@users.noreply.github.com>
2023-02-17 16:51:25 +0000

    added the tailwind

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,6 +4,7 @@ import "./src/styles/reset.css"
 import "./src/styles/variables.css"
 import "./src/styles/global.css"
 
+
 export const wrapRootElement = ({ element }) => (
   <StoreProvider>{element}</StoreProvider>
 )
